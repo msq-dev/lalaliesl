d71c5be1a143d68d1fa6db3f2abb938222966d21
msq <>
2021-10-24 13:47:46 +0200

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,89 @@
+Vue.component("current-track", {
+  props: {
+    currentTrack: Object
+  },
+  computed: {
+    artistsString() {
+      return this.currentTrack.item.artists.map(a => a.name).join(", ")
+    }
+  },
+  template: `<div class="current-track">
+               <div class="title">{{ currentTrack.item.name }}</div>
+               <div class="artists">{{ artistsString }}</div>
+             </div>`
+})
+
+
+Vue.component("searched-track", {
+  props: {
+    title: String,
+    artists: Array,
+  },
+  computed: {
+    artistsString() {
+      return this.artists.map(a => a.name).join(", ")
+    }
+  },
+  template: `<div class="searched-track">
+               <div class="title">{{ title }}</div>
+               <div class="artists">{{ artistsString }}</div>
+             </div>`
+})
+
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    access_token: "",
+    refresh_token: "",
+    loggedIn: false,
+    query: "",
+    searchResults: {},
+    currentTrack: {}   
+  },
+  methods: {
+    searchSpoti() {
+      let params = {q: this.query, type: "track,album,artist", limit: 50}
+      fetch("https://api.spotify.com/v1/search?" + new URLSearchParams(params), {
+        headers: {
+          "Authorization": "Bearer " + this.access_token
+        }
+      }).then(res => res.json())
+        .then(data => this.searchResults = data)
+        .catch(err => console.error(err))
+    },
+    getCurrentTrack() {
+      fetch("https://api.spotify.com/v1/me/player/currently-playing", {
+        headers: {
+          "Authorization": "Bearer " + this.access_token
+        }
+      }).then(res => res.json())
+        .then(data => this.currentTrack = data)
+        .catch(err => console.error(err))
+    }
+  },
+  mounted() {
+    let hashParams = {}
+    let e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1)
+    while (e = r.exec(q)) {
+      hashParams[e[1]] = decodeURIComponent(e[2])
+    }
+    
+    if (hashParams.error) {
+      alert('There was an error during the authentication')
+    } else {
+      if (hashParams.access_token) {
+        this.access_token = hashParams.access_token
+        this.refresh_token = hashParams.refresh_token
+        fetch("https://api.spotify.com/v1/me", {
+          headers: {
+            "Authorization": "Bearer " + this.access_token
+          }
+        }).then(res => this.loggedIn = true)
+          .then(res => this.getCurrentTrack())
+          .catch(err => console.error(err))
+      }
+    }
+  }
+})
---
+++ server.js
@@ -0,0 +1,138 @@
+const express = require('express')
+const request = require('request')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+
+const client_id = process.env.CLIENT_ID
+const client_secret = process.env.CLIENT_SECRET
+const redirect_uri = `${process.env.HOST}/callback`
+
+/**
+* Generates a random string containing numbers and letters
+* @param  {number} length The length of the string
+* @return {string} The generated string
+*/
+const generateRandomString = function(length) {
+  let text = ''
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
+}
+
+const stateKey = 'spotify_auth_state'
+
+const app = express()
+
+app.use(express.static(__dirname + '/public'))
+  .use(cors())
+  .use(cookieParser());
+
+app.get('/login', function(req, res) {
+
+  var state = generateRandomString(16)
+  res.cookie(stateKey, state)
+
+  // your application requests authorization
+  var scope = 'user-read-private user-read-email user-read-currently-playing'
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    new URLSearchParams({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }))
+})
+
+app.get('/callback', function(req, res) {
+
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  const code = req.query.code || null
+  const state = req.query.state || null
+  const storedState = req.cookies ? req.cookies[stateKey] : null
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      new URLSearchParams({
+        error: 'state_mismatch'
+      }))
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+      },
+      json: true
+    }
+
+    request.post(authOptions, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+
+        const access_token = body.access_token,
+            refresh_token = body.refresh_token;
+
+        const options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { 'Authorization': 'Bearer ' + access_token },
+          json: true
+        };
+
+        // use the access token to access the Spotify Web API
+        request.get(options, function(error, response, body) {
+          console.log(body)
+        });
+
+        // we can also pass the token to the browser to make requests from there
+        res.redirect('/#' +
+          new URLSearchParams({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          new URLSearchParams({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', function(req, res) {
+
+  // requesting access token from refresh token
+  const refresh_token = req.query.refresh_token;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  }
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token;
+      res.send({
+        'access_token': access_token
+      })
+    }
+  })
+})
+
+console.log('Listening on 8888')
+app.listen(8888)
